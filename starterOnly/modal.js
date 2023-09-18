64659f00cc29da394890656daa5a28676d7f5c6e
hzed <hzedek59@gmail.com>
2023-09-18 20:21:36 +0200

    Update modal.js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,11 +11,16 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-
 const closeBtn = document.querySelector(".close");
 const form = document.getElementById("form");
 const firstData = document.getElementById("first");
+const lastData = document.getElementById("last");
+const emailData = document.getElementById("email");
 const nomError = document.createElement("p");
+const nomError1 = document.createElement("p");
+const nomError2 = document.createElement("p");
+
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -39,26 +44,48 @@ function launchModal() {
 }
 
 
-// Ajout d'un écouteur d'événement sur le formulaire pour écouter le submit
-form.addEventListener("click", (event) => {
-  // On empêche le comportement par défaut
-event.preventDefault
-  // On fait la vérification.
-  if (firstData.value.trim() === "") {
-    nomError.textContent = "Veuillez remplir votre Nom ";
-    firstData.parentElement.appendChild(nomError);
-    nomError.classList.add("nom-error")
-  } else {
-    nomError.style.display = "none";
-  }
-});
+emailData.addEventListener("input", (event)=>{
+  let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+let resultat = regex.test(event.target.value);
+if (resultat === false) {
+  nomError2.style.display = "block";
+  nomError2.textContent = "Veuillez remplir votre email ";
+  emailData.parentElement.appendChild(nomError2);
+  nomError2.classList.add("nom-error")
 
-  // On fait la vérification.
-  if (firstData.value.trim() === "") {
-    nomError.textContent = "Veuillez remplir votre Nom ";
-    firstData.parentElement.appendChild(nomError);
-    nomError.classList.add("nom-error")
-  } else {
-    nomError.style.display = "none";
-  }
+} else {
+  nomError2.classList.remove("nom-error");
+  nomError2.style.display = "none";
+
+}})
+
+    // On fait la vérification.
+    lastData.addEventListener('input',()=>{
+      let lastDataValue = lastData.value.trim()
+      if (lastDataValue.length <= 2) {
+        nomError1.style.display = "block";
+        nomError1.textContent = "Veuillez remplir votre nom ";
+        lastData.parentElement.appendChild(nomError1);
+        nomError1.classList.add("nom-error")
+      } else {
+        nomError1.classList.remove("nom-error")
+        nomError1.style.display = "none";
+      }
+    })
+    firstData.addEventListener("input",()=>{
+    let firstDataValue = firstData.value.trim()
+    if (firstDataValue.length <= 2) {
+      nomError.style.display = "block";
+      nomError.textContent = "Veuillez remplir votre Prénom ";
+      firstData.parentElement.appendChild(nomError);
+      nomError.classList.add("nom-error")
+    } else {
+      nomError.style.display = "none";
+      nomError.classList.remove("nom-error")
+    }})
+// Ajout d'un écouteur d'événement sur le formulaire pour écouter le click
+form.addEventListener("submit", (event) => {
+    
+   event.preventDefault()
+  
 });
