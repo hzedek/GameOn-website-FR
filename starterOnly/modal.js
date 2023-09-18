cbd0efa74d69e401c4b92483355d6f3d45685e7d
hzed <hzedek59@gmail.com>
2023-09-18 11:40:41 +0200

    mineur changement

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,15 +7,25 @@ function editNav() {
   }
 }
 
-// DOM Elements
+// DOM element
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-const closeBtn = document.querySelector(".close");
 
+const closeBtn = document.querySelector(".close");
+const form = document.getElementById("form");
+const firstData = document.getElementById("first");
+const nomError = document.createElement("p");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Close modal event
+closeBtn.addEventListener("click", closeModal);
+
+function closeModal() {
+  modalbg.style.display = "none";
+}
 
 // CLose modal event
 closeBtn.addEventListener("click", closeModal)
@@ -28,3 +38,27 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+
+// Ajout d'un écouteur d'événement sur le formulaire pour écouter le submit
+form.addEventListener("click", (event) => {
+  // On empêche le comportement par défaut
+event.preventDefault
+  // On fait la vérification.
+  if (firstData.value.trim() === "") {
+    nomError.textContent = "Veuillez remplir votre Nom ";
+    firstData.parentElement.appendChild(nomError);
+    nomError.classList.add("nom-error")
+  } else {
+    nomError.style.display = "none";
+  }
+});
+
+  // On fait la vérification.
+  if (firstData.value.trim() === "") {
+    nomError.textContent = "Veuillez remplir votre Nom ";
+    firstData.parentElement.appendChild(nomError);
+    nomError.classList.add("nom-error")
+  } else {
+    nomError.style.display = "none";
+  }
+});
