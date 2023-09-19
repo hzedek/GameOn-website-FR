82b68dde70cbd5a8b6a68ab5d88503e646377242
hzed <hzedek59@gmail.com>
2023-09-19 08:30:18 +0200

    Update modal.js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -16,9 +16,14 @@ const form = document.getElementById("form");
 const firstData = document.getElementById("first");
 const lastData = document.getElementById("last");
 const emailData = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+
 const nomError = document.createElement("p");
 const nomError1 = document.createElement("p");
 const nomError2 = document.createElement("p");
+const nomError3 = document.createElement("p");
+const nomError4 = document.createElement("p");
 
 
 
@@ -28,9 +33,7 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // Close modal event
 closeBtn.addEventListener("click", closeModal);
 
-function closeModal() {
-  modalbg.style.display = "none";
-}
+function closeModal() {modalbg.style.display = "none";}
 
 // CLose modal event
 closeBtn.addEventListener("click", closeModal)
@@ -39,10 +42,37 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 // launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
-}
+function launchModal() {modalbg.style.display = "block";}
+    // On fait la vérification.
 
+    quantity.addEventListener('input',()=>{
+  let quantityValue = quantity.value;
+  let regex = /^[0-9]/;
+  if (regex.test(quantityValue) === false )
+   {
+    nomError4.style.display = "block";
+    nomError4.textContent = "Veuillez remplir le nombre de tournoi auquel vous avez participé ";
+    quantity.parentElement.appendChild(nomError4);
+    nomError4.classList.add("nom-error")
+  } else {
+    nomError4.classList.remove("nom-error")
+    nomError4.style.display = "none";
+  }
+})    
+
+birthdate.addEventListener('input',()=>{
+  let birthdateValue = Date.parse(birthdate.value)
+  if (birthdateValue <= Date.UTC(1920,9,19)
+  ) {
+    nomError3.style.display = "block";
+    nomError3.textContent = "Veuillez remplir votre date de naissance ";
+    birthdate.parentElement.appendChild(nomError3);
+    nomError3.classList.add("nom-error")
+  } else {
+    nomError3.classList.remove("nom-error")
+    nomError3.style.display = "none";
+  }
+})
 
 emailData.addEventListener("input", (event)=>{
   let regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
@@ -56,10 +86,8 @@ if (resultat === false) {
 } else {
   nomError2.classList.remove("nom-error");
   nomError2.style.display = "none";
-
 }})
 
-    // On fait la vérification.
     lastData.addEventListener('input',()=>{
       let lastDataValue = lastData.value.trim()
       if (lastDataValue.length <= 2) {
@@ -83,7 +111,7 @@ if (resultat === false) {
       nomError.style.display = "none";
       nomError.classList.remove("nom-error")
     }})
-// Ajout d'un écouteur d'événement sur le formulaire pour écouter le click
+// Ajout d'un écouteur d'événement sur le formulaire pour écouter le submit
 form.addEventListener("submit", (event) => {
     
    event.preventDefault()
